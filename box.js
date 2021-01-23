0c10c64a4f1c02180b6957c50f6f0b667e713900
DESNA2007 <76057325+DESNA2007@users.noreply.github.com>
2021-01-23 12:56:55 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,22 +1,25 @@
 class box{
-  constructor(x, y, width, height ) {
-    var options = {
-      restitution : 0.7,
-      friction : 0.6,
-      isStatic : false,
-    }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
-    World.add(world, this.body);
-  }
-  display(){
-    var angle = this.body.angle;
-    push();
-    translate(this.body.position.x, this.body.position.y);
-    rotate(angle);
-    rectMode(CENTER);
-    rect(0, 0, this.width, this.height);
-    pop();
-  }
+   constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+         
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
 }
