bdf34d8d6f30dcb5152d125a5305b9ab6d0e3b84
DESNA2007 <76057325+DESNA2007@users.noreply.github.com>
2021-01-23 12:59:57 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -1,31 +1,29 @@
 
   
-class SlingShot{
-    constructor(bodyA, pointB){
+class SlingShot{    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 20
-        }        
-        this.pointB = pointB
-        this.sling = Constraint.create(options);
-        World.add(world, this.sling);
-    }
-
-    fly(){
-        this.sling.bodyA = null;
-    }
-
-    display(){        
-        if(this.sling.bodyA){
-            var pointA = this.sling.bodyA.position;
-            var pointB = this.pointB;
-            
-            strokeWeight(4);
-            fill("red");
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            length: 1
         }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
     }
+    fly(){
+        this.sling.bodyA =null;
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("black");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
     
 }
